89dca355f6dd5bfb948a1961fda7ba98b3448a68
Han Jia <hanj@datacom.co.nz>
2016-07-27 09:52:10 +1200

    add ng-select

--- src/ng2App/wwwroot/app/components/itemofbusiness.component.ts
+++ src/ng2App/wwwroot/app/components/itemofbusiness.component.ts
@@ -3,11 +3,13 @@ import { ItemOfBusiness } from '../models/itemofbusiness';
 import { HearingOfEvidence } from '../models/HearingOfEvidence';
 import { HearingOfEvidenceComponent } from './hearingofevidence.component';
 import { Multiselect } from './plugins/multiselect';
+import { SELECT_DIRECTIVES } from './plugins/ng2-select/ng2-select';
+
 @Component({
     selector: 'item-of-business',
     templateUrl: `app/templates/item-of-business.html`,
     styles: [``],
-    directives: [HearingOfEvidenceComponent, Multiselect],
+    directives: [HearingOfEvidenceComponent, Multiselect, SELECT_DIRECTIVES],
     providers: []
 })
 export class ItemOfBusinessComponent implements OnInit {
@@ -17,6 +19,7 @@ export class ItemOfBusinessComponent implements OnInit {
 
     multiselectModel: Array<any> = [];
     subItemTypes: Array<any> = [];
+    selectedItem: any;
 
     public addSubItem = () => {
         let subItemType = this.multiselectModel['name'];
@@ -40,5 +43,57 @@ export class ItemOfBusinessComponent implements OnInit {
     }
     constructor() {
 
+    }
+
+
+
+    //public items: Array<string> = ['Amsterdam', 'Antwerp', 'Athens', 'Barcelona',
+    //    'Berlin', 'Birmingham', 'Bradford', 'Bremen', 'Brussels', 'Bucharest',
+    //    'Budapest', 'Cologne', 'Copenhagen', 'Dortmund', 'Dresden', 'Dublin', 'Düsseldorf',
+    //    'Essen', 'Frankfurt', 'Genoa', 'Glasgow', 'Gothenburg', 'Hamburg', 'Hannover',
+    //    'Helsinki', 'Leeds', 'Leipzig', 'Lisbon', 'Łódź', 'London', 'Kraków', 'Madrid',
+    //    'Málaga', 'Manchester', 'Marseille', 'Milan', 'Munich', 'Naples', 'Palermo',
+    //    'Paris', 'Poznań', 'Prague', 'Riga', 'Rome', 'Rotterdam', 'Seville', 'Sheffield',
+    //    'Sofia', 'Stockholm', 'Stuttgart', 'The Hague', 'Turin', 'Valencia', 'Vienna',
+    //    'Vilnius', 'Warsaw', 'Wrocław', 'Zagreb', 'Zaragoza'];
+
+    public items = [{ id: 1, text: "Hearing Of Evidence" }, { id: 2, text: "Consideration" }, { id: 3, text: "Free Text" }];
+
+    private value: any = ['Athens'];
+    private _disabledV: string = '0';
+    private disabled: boolean = false;
+
+    private get disabledV(): string {
+        return this._disabledV;
+    }
+
+    private set disabledV(value: string) {
+        this._disabledV = value;
+        this.disabled = this._disabledV === '1';
+    }
+
+    public selected(value: any): void {
+        this.selectedItem = value;
+    }
+
+    public removed(value: any): void {
+        this.selectedItem == null;
+    }
+
+    public refreshValue(value: any): void {
+        this.value = value;
+    }
+
+    public addItem = () => {
+        let subItemType = this.selectedItem['text'];
+        if (subItemType == "Hearing Of Evidence")
+            this.addHearing();
+    }
+
+    public itemsToString(value: Array<any> = []): string {
+        return value
+            .map((item: any) => {
+                return item.text;
+            }).join(',');
     }
 }
