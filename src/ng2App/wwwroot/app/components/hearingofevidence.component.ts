88dc1f254b284a742fd8936cb6ff54f969b9ea51
Han Jia <hanj@datacom.co.nz>
2016-08-02 13:17:35 +1200

    add modal component

--- src/ng2App/wwwroot/app/components/hearingofevidence.component.ts
+++ src/ng2App/wwwroot/app/components/hearingofevidence.component.ts
@@ -1,18 +1,48 @@
-﻿import { Component, OnInit, Input } from '@angular/core';
+﻿import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { HearingOfEvidence } from '../models/HearingOfEvidence';
+import { MODAL_DIRECTIVES, ModalComponent } from './plugins/ng2-bs3-modal/ng2-bs3-modal';
 
 @Component({
     selector: 'hearing-of-evidence',
     templateUrl: `app/templates/hearing-of-evidence.html`,
     styles: [``],
-    directives: [],
+    directives: [MODAL_DIRECTIVES],
     providers: []
 })
 export class HearingOfEvidenceComponent {
     @Input()
     hearing: HearingOfEvidence;
 
+    @ViewChild('modals')
+    modal: ModalComponent;
+
+    items: string[] = ['item1', 'item2', 'item3'];
+    animation: boolean = true;
+    keyboard: boolean = true;
+    backdrop: string | boolean = true;
+    css: boolean = false;
+
     constructor() {
 
+    }
+
+    opened() {
+        
+    }
+
+    navigate() {
+        
+    }
+
+    open() {
+        this.modal.open();
+    }
+
+    closed() {
+        
+    }
+
+    dismissed() {
+        
     }
 }
