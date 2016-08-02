0893de63918881a08c8ef6a25216d541a2b6f33b
Han Jia <hanj@datacom.co.nz>
2016-08-02 16:24:41 +1200

    add parameter for route

--- src/ng2App/wwwroot/app/components/routes/homeroute.ts
+++ src/ng2App/wwwroot/app/components/routes/homeroute.ts
@@ -1,12 +1,19 @@
 ﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app',
-    template: `<h3>Welcome to Home</h3>`,
+    template: `<h3>Welcome to Home</h3>
+                <button type="button" class="btn btn-default" (click)="open()">Open</button>`,
     styles: [],
     directives: [],
     providers: []
 })
 export class HomeRouteComponent {
-    
+    constructor(private router: Router) {
+    }
+
+    open = () => {
+        this.router.navigate(['/testroute', 18]);
+    }
 }
