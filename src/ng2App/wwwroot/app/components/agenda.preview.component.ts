6fdaff5c6a4f9e1050700a3aa0d693ac1402f162
Datacom <Datacom@CALLUMG-LT002>
2016-08-12 14:44:16 +1200

    print preview

--- src/ng2App/wwwroot/app/components/agenda.preview.component.ts
+++ src/ng2App/wwwroot/app/components/agenda.preview.component.ts
@@ -1,9 +1,19 @@
 ﻿import { Component, OnInit, Input, ContentChildren, AfterViewInit } from '@angular/core';
 import { Agenda } from '../models/agenda';
 import { ActivatedRoute, Router } from '@angular/router';
+
 @Component({
     selector: 'app',
-    template: `<h1>{{previewAgenda.Name}}</h1>`,
+    template: `<div class="container">
+                <div id="preview-detail">
+                <h1>Preview - Agenda</h1>
+                <span style="color: red">{{previewAgenda.Name}}</span>
+                </div>
+                <a href="#" class="btn btn-lg save-button pull-right" (click)="printAgenda()">
+                    <span class="glyphicon glyphicon-print"></span> Print
+                </a>
+                </div>
+                `,
     styles: [``],
     directives: [],
     providers: []
@@ -17,6 +27,9 @@ export class AgendaPreviewComponent implements OnInit {
         this.previewAgenda = JSON.parse(previewAgendaString);
     }
 
+    printAgenda() {
+        return xepOnline.Formatter.Format('preview-detail', { render: 'download' });
+    }
     
     constructor(private route: ActivatedRoute) {}
 }
