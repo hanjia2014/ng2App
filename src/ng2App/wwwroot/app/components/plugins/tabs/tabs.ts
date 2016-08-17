1fb197a55504e1133b6f20d8f9989e6b9e8612aa
Datacom <Datacom@CALLUMG-LT002>
2016-08-17 12:06:28 +1200

    fixed tab href and style

--- src/ng2App/wwwroot/app/components/plugins/tabs/tabs.ts
+++ src/ng2App/wwwroot/app/components/plugins/tabs/tabs.ts
@@ -6,11 +6,11 @@ import { Tab } from './tab';
     template: `
     <ul class="nav nav-tabs">
       <li *ngFor="let tab of tabs" (click)="selectTab(tab)" [class.active]="tab.active">
-        <a href="#">{{tab.title}}</a>
+        <a>{{tab.title}}</a>
       </li>
     </ul>
-    <ng-content></ng-content>
-  `
+    <ng-content></ng-content>`,
+    styles: [`a {cursor: pointer; cursor: hand;}`]
 })
 export class Tabs implements AfterContentInit {
     @ContentChildren(Tab) tabs: QueryList<Tab>;
